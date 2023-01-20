94fda602f780b9f3ca9188800854d1239d146118
Krutik Aghera <agherakrutik99@gmail.com>
2023-01-20 16:54:06 +0530

    Markdwon

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {marked} from "marked";
+import './App.css';
+
+marked.setOptions({
+  breaks: true,
+});
+
+const renderer = new marked.Renderer();
+
+
+
+function App() {
+  const [text,setText] = React.useState("");
+
+  return (
+    <div className="Text-center px-4">
+      <h1 className='p-4'>My Markdown Previewer</h1>
+      <textarea 
+        name='text' 
+        id='text' 
+        rows={10} 
+        value={text} 
+        className="textarea"
+        onChange={e => setText(e.target.value)} >
+    
+      </textarea>
+      <h3 className="mt-3">Output</h3>
+      <Preview markdown={text} />
+    </div>
+  );
+}
+
+function Preview({markdown}){
+  return (
+    <div dangerouslySetInnerHTML={{
+      __html: marked(markdown , {renderer: renderer}),
+    }} id="preview">
+    </div>
+  )
+}
+
+export default App;
