5be19336ae57ab074407e0fa7aefc93a8bbecb61
Krutik Aghera <agherakrutik99@gmail.com>
2023-01-27 22:38:10 +0530

    add default

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,36 @@ import React from "react";
 import {marked} from "marked";
 import './App.css';
 
+const defaultMarkdown = `# React Markdown Previewer
+
+## Type your Markdown in the Editor!
+<br><br>
+
+### Main functionality
+
+- Preview window updates real time with markdown syntax
+- The editor has some predefined input on page load
+- BONUS: Use &lt;br&gt; for line breaks
+
+<br>
+
+\`Is the syntax highlighting even working?\`
+\`\`\`javascript
+let s = "JavaScript syntax highlighting";
+alert(s);
+\`\`\`
+<br>
+
+> “If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.”
+― Marcus Aurelius, Meditations 
+<br>
+
+![react logo](https://i.postimg.cc/Bv9y8sBZ/react-logo.png)
+<br>
+
+Coded by **Stahlone**, 2019 for [freeCodeCamp](https://www.freecodecamp.org) Front End Libraries Challenges
+`;
+
 marked.setOptions({
   breaks: true,
 });
@@ -11,14 +41,14 @@ const renderer = new marked.Renderer();
 
 
 function App() {
-  const [text,setText] = React.useState("");
+  const [text,setText] = React.useState(defaultMarkdown);
 
   return (
     <div className="Text-center px-4">
       <h1 className='p-4'>My Markdown Previewer</h1>
       <textarea 
         name='text' 
-        id='text' 
+        id='editor' 
         cols={60}
         rows={10} 
         value={text} 
@@ -27,7 +57,7 @@ function App() {
     
       </textarea>
       <h3 className="mt-3">Output</h3>
-      <Preview markdown={text} />
+      <Preview id="preview" markdown={text} />
     </div>
   );
 }
